fd75613c32686a121b2eb10beee47b800ecf5481
eugene2050 <eugenioeugene09@gmail.com>
2023-08-07 17:52:18 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('portfolio.home-main');
+});
+
+
+Route::get('/test', function () {
+    return view('portfolio.home-bg-img');
+});
+
+Route::get('/test2', function () {
+    return view('portfolio.blog-details');
+});
+
+Route::get('/test3', function () {
+    return view('portfolio.home-dots-waves');
+});
+
+Route::get('/test4', function () {
+    return view('portfolio.home-liquid-bg');
+});
+
+Route::get('/test5', function () {
+    return view('portfolio.home-solid');
+});
+
+Route::get('/test6', function () {
+    return view('portfolio.project-details');
+});
